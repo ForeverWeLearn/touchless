3d0e0a1a717a099dcf70c409ad108b7dd8625d41
Fowel <theanh11052006@gmail.com>
2025-02-18 23:57:25 +0700

    refactor: to look better

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./assets/main.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
 
-import App from "./App.vue";
 import router from "./router";
+import App from "./App.vue";
 
 const app = createApp(App);
 
