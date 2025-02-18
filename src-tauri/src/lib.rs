e8e6cd2ca5d52a2cbdcc87e4fb55e635f11b7285
Fowel <theanh11052006@gmail.com>
2025-02-18 11:01:54 +0700

    feat: add function that move mouse by absolute coordnite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,22 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use mouce::{Mouse, MouseActions};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn move_mouse(pos: &str) {
+    let mouse = Mouse::new();
+    let coord: Vec<usize> = pos
+        .split_whitespace()
+        .map(|s| s.parse().expect("Failed to parse number"))
+        .collect();
+
+    let _ = mouse.move_to(coord[0].try_into().unwrap(), coord[1].try_into().unwrap());
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![move_mouse])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
