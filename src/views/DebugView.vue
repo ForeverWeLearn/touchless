567d231edd8dbf16ebd48ff1100740ab53b5176c
Fowel <theanh11052006@gmail.com>
2025-02-19 19:02:41 +0700

    feat: display hand gesture predict info

--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -1,14 +1,23 @@
+<script setup lang="ts">
+import { debug_view_store } from '../stores/debug_view';
+import HandResult from '../components/HandResult.vue';
+
+const debug_view = debug_view_store();
+</script>
+
 <template>
-  <div id="debug-container">
+  <div class="debug-container" :style="debug_view.style">
+    <HandResult :left="true">LEFT</HandResult>
     <div class="video-container">
-      <video id="webcam" autoplay playsinline></video>
-      <canvas id="webcam-overlay"></canvas>
+      <video width="640" height="480" id="webcam" autoplay playsinline></video>
+      <canvas width="640" height="480" id="webcam-overlay"></canvas>
     </div>
+    <HandResult :left="false">RIGHT</HandResult>
   </div>
 </template>
 
 <style scoped>
-#debug-container {
+.debug-container {
   margin-top: -100vh;
   display: flex;
   justify-content: center;
@@ -17,21 +26,22 @@
   height: 100vh;
   background-color: #191919;
 }
+
 .video-container {
   position: relative;
   margin-top: 60px;
   width: 640px;
   height: 480px;
 }
+
 video {
   transform: rotateY(180deg);
 }
+
 video,
 canvas {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
 }
 </style>
