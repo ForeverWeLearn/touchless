1b291577209e4b371243043f8d10a1fbfe7c05e2
Fowel <theanh11052006@gmail.com>
2025-02-19 19:11:07 +0700

    feat: logics change and reduce power usage when not recognize hands

--- src/scripts/engine.ts
+++ src/scripts/engine.ts
@@ -1,108 +1,78 @@
-import {
-  FilesetResolver,
-  HandLandmarker,
-  HandLandmarkerResult,
-  NormalizedLandmark,
-} from "@mediapipe/tasks-vision";
-import { GestureClassifier } from "./gesture_classifier";
+import { FilesetResolver, HandLandmarker, HandLandmarkerResult } from "@mediapipe/tasks-vision";
+import { left_hand_result_store, right_hand_result_store } from "../stores/hand_result";
 import { calc_keypoints, normalize_keypoints } from "../utils/algo";
 import { draw_connections, draw_landmarks } from "../utils/draw";
+import { GestureClassifier } from "./gesture_classifier";
+import { GestureParser } from "./gesture_parser";
 
 let results: HandLandmarkerResult;
+let next_trigger_time = 0;
 
 async function inference(engine: Engine) {
-  if (!engine.state) {
+  const current_time = performance.now();
+
+  if (current_time < next_trigger_time) {
+    window.requestAnimationFrame(() => inference(engine));
     return;
   }
 
-  if (engine.lastVideoTime !== engine.video.currentTime) {
-    engine.lastVideoTime = engine.video.currentTime;
-    results = engine.landmarker.detectForVideo(
-      engine.video,
-      performance.now()
-    ) as HandLandmarkerResult;
+  results = engine.landmarker.detectForVideo(engine.video, current_time);
+
+  engine.context_2d.clearRect(0, 0, engine.canvas.width, engine.canvas.height);
+
+  if (results.landmarks.length == 0) {
+    next_trigger_time = current_time + 250;
+    window.requestAnimationFrame(() => inference(engine));
+    return;
   }
 
-  engine.canvasCtx.clearRect(0, 0, engine.canvas.width, engine.canvas.height);
-  if (results.landmarks.length > 0) {
-    let i = 0;
-    for (const landmark of results.landmarks) {
-      let keypoints = calc_keypoints(
-        landmark as NormalizedLandmark[],
-        engine.canvas
-      ) as number[][];
+  let i = 0;
+  for (const landmark of results.landmarks) {
+    let keypoints = calc_keypoints(landmark, engine.canvas);
 
-      draw_connections(
-        keypoints,
-        engine.canvasCtx,
-        engine.lineColor,
-        engine.lineThickness
-      );
-      draw_landmarks(
-        keypoints,
-        engine.canvasCtx,
-        engine.landmarkColor,
-        engine.landmarkRadius
-      );
+    engine.draw(keypoints);
 
-      let normalized_keypoints = normalize_keypoints(keypoints);
+    let normalized_keypoints = normalize_keypoints(keypoints);
 
-      let labelID = -1;
-      if (results.handedness[i][0].displayName == "Right") {
-        labelID =
-          engine.gesture_classifier_right.inference(normalized_keypoints);
-      } else {
-        labelID =
-          engine.gesture_classifier_left.inference(normalized_keypoints);
-      }
+    const handedness = results.handedness[i][0].displayName == "Left" ? 0 : 1;
+    const label_id = engine.gesture_classifier[handedness].inference(normalized_keypoints);
 
-      console.log(engine.labels[labelID]);
+    engine.gesture_parser[handedness].parse(label_id, keypoints);
 
-      i += 1;
-    }
+    engine.hand_result[handedness].gesture = engine.gesture_parser[handedness].labels[label_id];
+    engine.hand_result[handedness].hold_time = engine.gesture_parser[handedness].hold_time;
+
+    i += 1;
   }
 
   window.requestAnimationFrame(() => inference(engine));
 }
 
 export class Engine {
-  public labels: string[] = [];
   public landmarker!: HandLandmarker;
-  public gesture_classifier_left!: GestureClassifier;
-  public gesture_classifier_right!: GestureClassifier;
+  public gesture_classifier: GestureClassifier[] = [];
+  public gesture_parser: GestureParser[] = [new GestureParser(), new GestureParser()];
+  public hand_result = [left_hand_result_store(), right_hand_result_store()];
 
   public video!: HTMLVideoElement;
   public canvas!: HTMLCanvasElement;
-  public canvasCtx!: CanvasRenderingContext2D;
+  public context_2d!: CanvasRenderingContext2D;
 
-  public lastVideoTime = -1;
-  public state = false;
-
-  public landmarkColor = "#f9756d";
-  public lineColor = "#32b78e";
-  public landmarkRadius = 2;
-  public lineThickness = 2;
+  public landmark_color = "#ff0022";
+  public line_color = "#001f22";
+  public landmark_radius = 5;
+  public line_thickness = 3;
 
   private stream!: MediaStream;
-  private inferenceHandler!: () => void;
+  private inference_handler!: () => void;
 
   constructor() {
-    this.readLabels();
-    this.loadModel();
-    this.findAndConnectCamera();
+    this.load_model();
   }
 
-  private readLabels() {
-    fetch("labels.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.labels.push(...data);
-      });
-  }
-
-  private async loadModel() {
-    const filesetResolver = await FilesetResolver.forVisionTasks("wasm");
-    this.landmarker = await HandLandmarker.createFromOptions(filesetResolver, {
+  private async load_model() {
+    const fileset_resolver = await FilesetResolver.forVisionTasks("wasm");
+    this.landmarker = await HandLandmarker.createFromOptions(fileset_resolver, {
       baseOptions: {
         modelAssetPath: "hand_landmarker.task",
         delegate: "GPU",
@@ -110,40 +80,34 @@ export class Engine {
       runningMode: "VIDEO",
       numHands: 2,
     });
-    this.gesture_classifier_left = new GestureClassifier(
-      "gesture_classifier_left.tflite"
-    );
-    this.gesture_classifier_right = new GestureClassifier(
-      "gesture_classifier_right.tflite"
-    );
+    this.gesture_classifier[0] = new GestureClassifier("gesture_classifier_left.tflite");
+    this.gesture_classifier[1] = new GestureClassifier("gesture_classifier_right.tflite");
   }
 
-  private findAndConnectCamera() {
+  private connect_camera() {
     const video = document.getElementById("webcam") as HTMLVideoElement;
     if (video == null) {
       return;
     }
     this.video = video as HTMLVideoElement;
 
-    const canvas = document.getElementById(
-      "webcam-overlay"
-    ) as HTMLCanvasElement;
+    const canvas = document.getElementById("webcam-overlay") as HTMLCanvasElement;
     if (canvas == null) {
       return;
     }
     this.canvas = canvas;
-    this.canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.context_2d = canvas.getContext("2d") as CanvasRenderingContext2D;
 
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
       this.video.srcObject = stream;
       this.stream = stream;
-      this.inferenceHandler = () => inference(this);
-      this.video.addEventListener("loadeddata", this.inferenceHandler);
+      this.inference_handler = () => inference(this);
+      this.video.addEventListener("loadeddata", this.inference_handler);
     });
   }
 
-  private disconnectCamera() {
-    this.video.removeEventListener("loadeddata", this.inferenceHandler);
+  private disconnect_camera() {
+    this.video.removeEventListener("loadeddata", this.inference_handler);
     if (this.video.srcObject) {
       this.video.pause();
       this.video.srcObject = null;
@@ -151,15 +115,19 @@ export class Engine {
         track.stop();
       });
     }
-    this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context_2d.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  public setState(state: boolean) {
-    this.state = state;
+  public set_state(state: boolean) {
     if (state) {
-      this.findAndConnectCamera();
+      this.connect_camera();
     } else {
-      this.disconnectCamera();
+      this.disconnect_camera();
     }
+  }
+
+  public draw(keypoints: number[][]) {
+    draw_connections(keypoints, this.context_2d, this.line_color, this.line_thickness);
+    draw_landmarks(keypoints, this.context_2d, this.landmark_color, this.landmark_radius);
   }
 }
